0e2eeee98762e51c874c4b555790d2186c29f4f9
Mathew Payne <2772944+GeekMasher@users.noreply.github.com>
2024-12-04 14:12:09 +0000

    fix: Debugging to setup
    - Default is false

--- ghasreview/__main__.py
+++ ghasreview/__main__.py
@@ -2,4 +2,4 @@ from ghasreview.app import create_app, config
 
 if __name__ == "__main__":
     app = create_app(config)
-    app.run("0.0.0.0", port=9000, debug=True)
+    app.run("0.0.0.0", port=9000, debug=config.get("GHAS_DEBUG", False))
